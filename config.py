cd7bc2f399d841f0c927ae6bdd384ed0afa86f3e
takhirovulugbek777 <129583619+takhirovulugbek777@users.noreply.github.com>
2023-08-04 14:55:18 +0500

    Added config

---
+++ config.py
@@ -0,0 +1,13 @@
+API_TOKEN = '6637522214:AAGJDRpP_-rzMlb-VI5fm-4Yai0iqEwg80U'
+
+DB_NAME = 'quramiz'
+DB_USER = 'postgres'
+DB_HOST = 'localhost'
+DB_PASSWORD = '1234'
+
+# ------------ stroy materyal -------------------------
+
+API = f"https://quramiz.uz/api/product2/filter?catalog="
+DETAIL_API = 'https://quramiz.uz/api/product2/'
+
+ADMIN = [1651940013, 124118340]
