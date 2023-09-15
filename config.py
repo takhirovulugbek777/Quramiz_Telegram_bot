8960a4c2d5a588dc76e3caa8e35748ce3c794865
takhirovulugbek777 <129583619+takhirovulugbek777@users.noreply.github.com>
2023-09-15 16:22:42 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-API_TOKEN = '6637522214:AAGJDRpP_-rzMlb-VI5fm-4Yai0iqEwg80U'
+API_TOKEN = 'token'
 
 DB_NAME = 'quramiz_db'
-DB_USER = 'quramiz_db_user'
-DB_HOST = 'dpg-ck23mmf03lhc73f31tv0-a'
-DB_PASSWORD = 'Uvyb5ayNGDvHgmdQihzFMMD3G40LcbCW'
-DB_PORT = '5432'
+DB_USER = 'quramiz'
+DB_HOST = 'host'
+DB_PASSWORD = 'password'
+DB_PORT = 'port'
 
 # ------------ stroy materyal -------------------------
 
-API = f"https://quramiz.uz/api/product2/filter?catalog="
-DETAIL_API = 'https://quramiz.uz/api/product2/'
+API = f"api"
+DETAIL_API = 'detail api'
 
 ADMIN = []
