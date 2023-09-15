fe57de8ba3240cad76a77c81c256e3f73a2d458f
takhirovulugbek777 <129583619+takhirovulugbek777@users.noreply.github.com>
2023-09-15 16:15:43 +0500

    Update config.py
    add port sor server

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 API_TOKEN = '6637522214:AAGJDRpP_-rzMlb-VI5fm-4Yai0iqEwg80U'
 
-DB_NAME = 'quramiz'
-DB_USER = 'postgres'
-DB_HOST = 'localhost'
-DB_PASSWORD = '1234'
+DB_NAME = 'quramiz_db'
+DB_USER = 'quramiz_db_user'
+DB_HOST = 'dpg-ck23mmf03lhc73f31tv0-a'
+DB_PASSWORD = 'Uvyb5ayNGDvHgmdQihzFMMD3G40LcbCW'
+DB_PORT = '5432'
 
 # ------------ stroy materyal -------------------------
 
